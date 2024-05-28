18968f5019c27aeefad3d8e9815e1135a1adf594
kichul7493 <kichul7493@gmail.com>
2024-05-28 12:52:01 +0900

    feat: 페이지 접속 인가 미들웨어 설정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,13 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+
+export default auth((req) => {
+  if (!req.auth) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/login";
+    return Response.redirect(url);
+  }
+});
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)", "/"],
+  matcher: ["/post/create/:path*"],
 };
