c6a67ebd5fe8cfd27d9ccfa399cb58045adfba49
kichul7493 <kichul7493@gmail.com>
2024-05-31 15:25:17 +0900

    fix: 태그 리스트 좌우 스크롤 적용

--- src/components/home/tagList/TagList.tsx
+++ src/components/home/tagList/TagList.tsx
@@ -6,24 +6,26 @@ const TagList = async () => {
   const tags = await getTags();
 
   return (
-    <div className="px-7 flex gap-4 overflow-x-auto py-5 mb-5 scrollbar-hidden">
-      <Link
-        href={`/?tag=all`}
-        className="min-w-7 text-center text-xl font-medium text-gray100"
-      >
-        All
-      </Link>
-
-      {tags.map((tag) => (
+    <div className="px-7 overflow-x-auto py-5 mb-5 scrollbar-hidden">
+      <div className="w-[200vw] flex gap-4">
         <Link
-          key={tag.id}
-          href={`/?tag=${tag.name}`}
-          prefetch={true}
+          href={`/?tag=all`}
           className="min-w-7 text-center text-xl font-medium text-gray100"
         >
-          {tag.name}
+          All
         </Link>
-      ))}
+
+        {tags.map((tag) => (
+          <Link
+            key={tag.id}
+            href={`/?tag=${tag.name}`}
+            prefetch={true}
+            className="min-w-7 text-center text-xl font-medium text-gray100"
+          >
+            {tag.name}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
